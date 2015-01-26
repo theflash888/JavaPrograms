2e1ad9df5d22092f438bd0be441393a8a8fd6495
theflash888 <mohamad@poetdisplays.com>
2015-01-26 08:38:56 -0500

    Update FillArray.java

--- FillArray.java
+++ FillArray.java
@@ -4,15 +4,19 @@ public class CreateArray
 {
 	public static void main(String[] args)
 	{
+		//specify length of array
 		int LENGTH = 10;
 		
+		//create array
 		int[] numbers = new int[LENGTH];
 		
+		//loop through each array element and fill it with 0-9 
 		for(int i = 0; i < LENGTH; i++)
 		{
 			numbers[i] = i;
 		}
 		
+		//print each element in the array
 		for(int i = 0; i < LENGTH; i++)
 			System.out.println(numbers[i]);
 	}
